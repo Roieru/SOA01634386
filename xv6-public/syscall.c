f7ad175fb06c12bc37478fbd11dcaa6f1759afdc
Roieru <a01634386@itesm.mx>
2019-09-11 01:02:35 +0000

    corrected print output

--- xv6-public/syscall.c
+++ xv6-public/syscall.c
@@ -166,7 +166,7 @@ syscall(void)
 
   num = curproc->tf->eax;
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
-    cprintf("System call is %s", syscallnames[num]);
+    cprintf("%s -> %d\n", syscallnames[num],num);
     curproc->tf->eax = syscalls[num]();
   } else {
     cprintf("%d %s: unknown sys call %d\n",
